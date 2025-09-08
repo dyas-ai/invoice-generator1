d1b46461740a7b13de31d7b84077a3ed2648b24d
dyas-ai <sridharanaaryan@gmail.com>
2025-09-08 16:50:39 +0530

    Update 8app.py

--- 8app.py
+++ 8app.py
@@ -136,9 +136,8 @@ def generate_proforma_invoice(df, form_data):
 
     styles = getSampleStyleSheet()
     title_style = ParagraphStyle('Title', parent=styles['Normal'], fontSize=12,
-                                 alignment=TA_CENTER, fontName='Helvetica-Bold', spaceAfter=0,
-                                 borderWidth=1, borderColor=colors.black, borderPadding=6,
-                                 leftIndent=24, rightIndent=24)
+                                 alignment=TA_CENTER, fontName='Helvetica-Bold', spaceAfter=6,
+                                 borderWidth=1, borderColor=colors.black, borderPadding=6)
     header_style = ParagraphStyle('Header', parent=styles['Normal'], fontSize=7,
                                   fontName='Helvetica-Bold', alignment=TA_LEFT, 
                                   spaceBefore=0, spaceAfter=0, leading=8)
